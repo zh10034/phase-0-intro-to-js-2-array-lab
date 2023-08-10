413a430c7a1de1f0861a307445b509b72c482796
zh10034 <zh10034@LAPTOP-KH5314B2.localdomain>
2023-08-10 18:20:54 -0400

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name){
+cats.push("Ralph")
+};
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat(name){
+    cats.pop("Garfield")
+};
+function destructivelyRemoveFirstCat(name){
+    cats.shift("Milo")
+};
+function appendCat(name){
+    let copyOfCats = cats.slice();
+    copyOfCats.push("Broom")
+    return copyOfCats
+};
+function prependCat(name){
+    let copyOfCats = cats.slice();
+    copyOfCats.unshift("Arnold")
+    return copyOfCats
+};
+function removeLastCat(name){
+    let copyOfCats = cats.slice();
+    copyOfCats.pop("Garfield");
+    return copyOfCats
+};
+function removeFirstCat(name){
+    let copyOfCats = cats.slice();
+    copyOfCats.shift("Milo");
+    return copyOfCats
+};
+
+
+
+
+
+
+
